c06092bfd2a282187868f38dd30c3c4851317581
Naguru Sai Siddartha Reddy <saisiddarthanaguru@gmail.com>
2026-07-02 17:47:43 +0530

    Refactor event ingestion logic for clarity

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import os
 
 app = FastAPI(title="Analytics Ingestion Engine")
 
-# Connect to Redis container using its Docker service name 'redis'
+
 REDIS_HOST = os.getenv("REDIS_HOST", "redis")
 redis_client = redis.Redis(host=REDIS_HOST, port=6379, db=0, decode_responses=True)
 
@@ -17,10 +17,10 @@ class EventSchema(BaseModel):
 @app.post("/events", status_code=202)
 def ingest_event(event: EventSchema):
     try:
-        # Convert Pydantic object to JSON string
+        
         event_data = json.dumps(event.model_dump())
         
-        # Push to the back of the Redis list (Queue)
+        
         redis_client.rpush("event_queue", event_data)
         
         return {"status": "Accepted", "message": "Event queued successfully."}
